f38f749bd8b1c72727fed1a08e5626e8e1d4be51
monkeyschool <58121490+monkeyschool@users.noreply.github.com>
2020-02-22 21:56:36 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,82 +1,156 @@
-# Info File http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-# Data files https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-# R script run_analysis.R does the following:
-### 1. Merges the training and the test sets to create one data set.
-### 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-### 3. Uses descriptive activity names to name the activities in the data set
-### 4. Appropriately labels the data set with descriptive variable names.
-### 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-#
-#
-########################
-# download data - verify directory - unzip contents
-fileName <- "projectdata.zip"
-url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-dir <- "UCI HAR Dataset"
-#
-# 
-if(!file.exists(fileName)){
-        download.file(url,fileName, mode = "wb") 
-}
-# 
-if(!file.exists(dir)){
-        unzip("projectdata.zip", files = NULL, exdir=".")
-}
-########################
-# read all files 
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
-X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-#
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
-X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-#
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
-features <- read.table("UCI HAR Dataset/features.txt")
-#########################
-#
-### 1. Merges the training and the test sets to create one data set.
-extract_mean_std <- rbind(X_train,X_test)
-#
-### 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# Use the vector to subset data from the mean and standard deviation of each measurement.
-mean_std <- grep("mean()|std()", features[, 2]) 
-extract_mean_std <- extract_mean_std[,mean_std]
-#
-### 3. Uses descriptive activity names to name the activities in the data set
-### 4. Appropriately labels the data set with descriptive variable names.
-# using gsub remove "()" 
-activity_names <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
-names(extract_mean_std) <- activity_names[mean_std]
-# combine the test and train and apply to name the activities
-subject <- rbind(subject_train, subject_test)
-names(subject) <- 'subject'
-activity <- rbind(y_train, y_test)
-names(activity) <- 'activity'
-# 
-# group the activity column of dataSet, re-name lable of levels with activity_levels, and apply it to dataSet.
-act_group <- factor(dataSet$activity)
-levels(act_group) <- activity_labels[,2]
-dataSet$activity <- act_group
-# combine subject, activity, and mean and std only data set to create final data set.
-extract_mean_std <- cbind(subject,activity, extract_mean_std)
-#
-# group the activity column of dataSet, re-name lable of levels with activity_levels, and apply it to dataSet.
-act_group <- factor(extract_mean_std$activity)
-levels(act_group) <- activity_labels[,2]
-dataSet$activity <- act_group
-#
-### 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-# check if reshape2 package is installed
-if (!"reshape2" %in% installed.packages()) {
-        install.packages("reshape2")
-}
-library("reshape2")
+## The script loads the test / training datasets and merges them in a tidy dataset
 
-# melt and write the tidy data to "tidy_data.txt"
-baseData <- melt(extract_mean_std,(id.vars=c("subject","activity")))
-secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
-names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
-write.table(secondDataSet, "tidy_data.txt", sep = ",")
-#
+
+## 
+## load the features.txt dataset
+## 
+loadFeatures <- function(dir) {
+    fileName = paste(dir,"/features.txt",sep="")
+    colName = c("num","label")
+    tbl_features <- read_delim(fileName, delim = " ", col_names = colName)
+}
+##
+## load the raw dataset
+##
+loadDataset <- function(dir, type, featlbl) {
+    fileName = filePath(dir, "subject", type)
+    colName = "subject"
+    tbl_subject <- read_delim(fileName, delim = " ", col_names = colName)
+    # add new column 'type' (values = [test,train])
+    nbRows = nrow(tbl_subject)
+    tbl_subject <- add_column(tbl_subject, 'type' = c(rep(type,nbRows))) 
+    # labels (activities)
+    fileName = filePath(dir, "y", type)
+    colName = "activity"
+    tbl_y <- read_delim(fileName, delim = " ", col_names = colName)
+    # This part uses descriptive activity names
+    tbl_y <- mutate(tbl_y, activity = recode(activity,
+                                        `1` = "WALKING",
+                                        `2` = "WALKING_UPSTAIRS",
+                                        `3` = "WALKING_DOWNSTAIRS",
+                                        `4` = "SITTING",
+                                        `5` = "STANDING",
+                                        `6` = "LAYING"))
+
+
+    # load X dataset
+    fileName = filePath(dir, "X", type)
+    colName = featlbl
+    tbl_X <- read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+
+    nb_signals = 128
+    subdirdata = "Inertial Signals"
+    # body_acc_x/y/z
+    datasetname = "body_acc_x"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_acc_x = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "body_acc_y"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_acc_y = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "body_acc_z"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_acc_z = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    # body_gyro_x/y/z
+    datasetname = "body_gyro_x"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_gyro_x = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "body_gyro_y"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_gyro_y = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "body_gyro_z"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_body_gyro_z = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    # total_acc_x/y/z
+    datasetname = "total_acc_x"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_total_acc_x = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "total_acc_y"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_total_acc_y = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    datasetname = "total_acc_z"
+    fileName = filePath(dir, paste(subdirdata,datasetname, sep="/"), type)
+    colName = paste(rep(datasetname, nb_signals),sprintf("%03d",seq(from = 1, by = 1, length.out = nb_signals)), sep = "_")
+    tbl_total_acc_z = read_delim(fileName, delim = " ", col_names = colName, col_types = cols(.default = "n") )
+    
+    ## merge dataset
+    tbl_test <- bind_cols(tbl_subject,
+                          tbl_y,
+                          tbl_X,
+                          tbl_body_acc_x,
+                          tbl_body_acc_y,
+                          tbl_body_acc_z,
+                          tbl_body_gyro_x,
+                          tbl_body_gyro_y,
+                          tbl_body_gyro_z,
+                          tbl_total_acc_x,
+                          tbl_total_acc_y,
+                          tbl_total_acc_z)
+}
+
+## 
+## build a full file path
+## 
+filePath <- function(dir, fileName, type) {
+
+    paste(dir,"/",type,"/",paste(fileName,"_",type,sep=""),".txt",sep="")
+    
+}
+
+# use 'readr' library to make directly some tibble
+library(readr)
+library(tibble)
+library(dplyr)
+
+# my local repository of initial dataset
+setwd("~/R/03 - Geting and Cleaning Data")
+
+##
+## Merges the training and the test sets to create one data set.
+## 
+
+## Load Test dataset
+folderName <- "./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset"
+
+# load features with labels (only once)
+ft_lbl<-loadFeatures(folderName)
+
+# dataset TEST
+typeOfDataSet <- "test"
+dt_test<-loadDataset(folderName, typeOfDataSet, ft_lbl$label)
+
+# dataset TRAINING
+typeOfDataSet <- "train"
+dt_training<-loadDataset(folderName, typeOfDataSet, ft_lbl$label)
+
+# global merge
+dt_total <- bind_rows(dt_test, dt_training)
+
+## 
+## Extracts only the measurements on the mean and standard deviation for each measurement.
+## 
+dt_extract <- dt_total[grepl("subject|activity|-mean|-std", names(dt_total))]
+
+## Uses descriptive activity names to name the activities in the data set
+## 
+## Creates a second, independent tidy data set with the avg of each variable for each activity and each subject
+## -----------------------------------------------------------------
+gby_subject_activity <- group_by(dt_extract, subject, activity)
+dt_stats <- summarise_all(gby_subject_activity, tibble::lst(mean))
+write.table(dt_stats, file="dt_stats.txt", row.name=FALSE)
+
